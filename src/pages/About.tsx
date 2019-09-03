a0224339bb24a257a61f66b12b7ecad7609c5950
Sarthak Bhandari <sarthak1657@gmail.com>
2019-09-02 21:19:51 -0400

    feat: 🎸 Added home component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@material-ui/core";
 import React from "react";
 
 const About = () => {
-    return (<Typography paragraph>Hello</Typography>);
+    return (<Typography paragraph>About Me</Typography>);
 };
 
 export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+const Home = () => {
+    return (<Typography paragraph>Hello, Sarthak here. I'm a ---developer.---</Typography>);
+};
+
+export default Home;
